83071ea58c41a6dbd6d2162bc190799b1f9d0401
Qbuiba <37890918+Qbuiba@users.noreply.github.com>
2024-04-06 10:07:17 -0500

    solution

---
+++ src/main/java/miu/ea/group3g/lab4/entity/User.java
@@ -0,0 +1,27 @@
+package miu.ea.group3g.lab4.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    @JsonManagedReference
+    @Fetch(FetchMode.SELECT)
+//    @BatchSize(size = 10)
+    private List<Review> reviews;
+}
